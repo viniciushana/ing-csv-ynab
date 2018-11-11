23cf155832bd028d16fdcaa970ad07a2cfadf7ca
Vinicius Hana <viniciushanas@gmail.com>
2018-11-11 10:31:48 +0100

    First commit

---
+++ cmd/convert.go
@@ -0,0 +1,47 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// convertCmd represents the convert command
+var convertCmd = &cobra.Command{
+	Use:   "convert [ing csv file] [desired output file name]",
+	Short: "Converts an ING CSV file to a YNAB-compliant one",
+	Long: `Takes a CSV bank statement from ING, reads its data and outputs it in a format and disposition
+that YNAB is able to read.`,
+	Args: cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("convert called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(convertCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// convertCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// convertCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
